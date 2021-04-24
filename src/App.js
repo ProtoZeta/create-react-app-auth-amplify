693c0f9d75e8556409c18b0aa8313b29f154696f
ProtoZeta <infinitereload@gmail.com>
2021-04-24 14:38:48 +0700

    different page route via url test

--- src/App.js
+++ src/App.js
@@ -7,36 +7,47 @@ import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 import Main from './screen/main'
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reloads.
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </header>
-//       </div>
-//       <Main/>
-//     );
-//   }
-// }
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-export default function App() {
-  return (
-    <View>
-      <Home/>
-    </View>
-  );
+class App extends Component {
+  render() {
+    return (
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reloads.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+      // <Main/>
+      <Router>
+        <div>
+          <ul>
+            <Link to="/home">Home</Link>
+          </ul>
+          <Switch>
+            <Route exact path="/home">
+              <Main/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default withAuthenticator(App, true);
